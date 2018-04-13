9ba4140dabe21cfcd516cf82a41022c9f5c7359f
Pepe Valdivia <jvaldivia@softweb.pe>
2018-04-13 15:23:55 -0500

    Prueba de carga jmeter añadida

--- config/database.js
+++ config/database.js
@@ -1,17 +1,38 @@
 const Sequelize = require('sequelize');
+const constants = require('./constants');
 
-const sequelize = new Sequelize('database', 'username', 'password', {
-	//host: 'localhost',
-	dialect: 'sqlite',
-	pool: {
-		 max: 5,
-		 min: 0,
-		 idle: 10000
-	},
-	storage: 'db/ubicaciones.db',
-	define: {
-		timestamps: false // true by default
-	}
-});
+if(constants.data.ambiente == 'desarrollo'){
+	const sequelize = new Sequelize('database', 'username', 'password', {
+		//host: 'localhost',
+		dialect: 'sqlite',
+		pool: {
+			 max: 5,
+			 min: 0,
+			 idle: 10000
+		},
+		storage: 'db/ubicaciones.db',
+		define: {
+			timestamps: false // true by default
+		},
+		logging: true,
+	});
+	exports.db = sequelize;
+}
 
-exports.db = sequelize;
+if(constants.data.ambiente == 'produccion'){
+	const sequelize = new Sequelize('database', 'username', 'password', {
+		//host: 'localhost',
+		dialect: 'sqlite',
+		pool: {
+			 max: 5,
+			 min: 0,
+			 idle: 10000
+		},
+		storage: 'db/ubicaciones.db',
+		define: {
+			timestamps: false // true by default
+		},
+		logging: false,
+	});
+	exports.db = sequelize;
+}
