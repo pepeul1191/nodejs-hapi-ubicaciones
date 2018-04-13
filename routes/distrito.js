3aca5152ef3b3dde2b2ae36566b88c64a2aaceda
Pepe Valdivia <jvaldivia@softweb.pe>
2018-04-13 10:02:53 -0500

    Obtener distrito,provnica,departamento por distrito_id

--- routes/distrito.js
+++ routes/distrito.js
@@ -61,4 +61,32 @@ module.exports = [
       });
     }
   },
+  {
+    method: 'GET',
+    path: 'nombre/{distrito_id}',
+    config: {
+      auth: false,
+      pre: [
+      ],
+    },
+    handler: function(request, reply) {
+      models.DistritoProvinciaDepartamento.findOne({
+        attributes: ['id', 'nombre'],
+        where: {
+          id : request.params.distrito_id
+        },
+      }).then(function(distritos) {
+        reply(JSON.stringify(distritos));
+      }).catch((err) => {
+        var rpta = {
+          'tipo_mensaje': 'error',
+          'mensaje': [
+            'Se ha producido un error en obtener el distrito',
+            err.toString()
+          ]
+        }
+        reply(JSON.stringify(rpta)).code(500);
+      });
+    }
+  },
 ]
